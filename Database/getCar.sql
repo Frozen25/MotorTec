167d29c676e718fe9ee9a903b831762fea962c5e
Unknown <aguisnchez@gmail.com>
2018-09-27 02:09:12 -0600

    Elimina storedprocedures innecesarios

--- Database/getCar.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getCar`(
-IN _idCoche INT
-)
-BEGIN
-
-	SELECT c.idCoche, c.Matricula, c.Modelo, c.Color, c.Estado, c.Image, c.Kilometraje, m.Nombre AS Marca 
-    FROM Coche c JOIN Marca m ON c.Marca_idMarca = m.idMarca 
-    WHERE c.idCoche = _idCoche;
-END
